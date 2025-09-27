6a5cb16c9c09c06b400a6f6d328c226714f78dae
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-09-27 23:06:03 +0600

    dist added

--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -6,7 +6,7 @@ const Toggle = ({toggle,setToggle,title,setTitle,purchase}) => {
             <div className='flex justify-between font-bold my-5'><h1 className='text-2xl'>{title===true?`Selected player ${purchase.length}/6`:"Available Player"}</h1>
         <div><button className={`border-1 border-gray-400 p-3 rounded-xl border-r-0 ${toggle===true?"bg-yellow-300":""}`} onClick={()=>{setToggle(true)
             setTitle(false)
-        }}>Available player </button>
+        }}>Available players </button>
         <button className={`border-1 border-gray-400 p-3 rounded-xl border-l-0 ${toggle===false?"bg-yellow-300":""}`}onClick={()=>{setToggle(false)
             setTitle(true)
         }}>Selected Player <span>{`${purchase.length}`}</span></button>
